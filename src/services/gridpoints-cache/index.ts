e29e9f06f62c56baeb87104fb0eda0d5db755751
gustavofabro <gustavofabro.f@gmail.com>
2023-03-05 16:58:12 -0300

    feat/weather: rename file

--- src/services/gridpoints-cache/index.ts
+++ src/services/gridpoints-cache/index.ts
@@ -2,14 +2,14 @@ import { GridPoints } from 'domain/weather-grid-points.model';
 
 const LOCAL_STORAGE_WEATHER_CHALLENGE_KEY = 'weather-challenge-react';
 
-function getGridPointsLocalStorage(latLong: {lat: string, lng: string}): GridPoints | null {
+function getGridPointsCache(latLong: {lat: string, lng: string}): GridPoints | null {
   const data =  JSON.parse(localStorage.getItem(LOCAL_STORAGE_WEATHER_CHALLENGE_KEY) || '{}');
   const gridPointsForLatLong = data[_getPropertieKey(latLong)];
 
   return gridPointsForLatLong || null;
 }
 
-function saveGridPointsLocalStorage(latLong: {lat: string, lng: string}, gridPoints: GridPoints): void {
+function saveGridPointsCache(latLong: {lat: string, lng: string}, gridPoints: GridPoints): void {
   let data =  JSON.parse(localStorage.getItem(LOCAL_STORAGE_WEATHER_CHALLENGE_KEY) || '{}');
 
   data = {
@@ -24,4 +24,4 @@ function _getPropertieKey(latLong: {lat: string, lng: string}): string {
   return `${latLong.lat}/${latLong.lng}`;
 }
 
-export { getGridPointsLocalStorage, saveGridPointsLocalStorage };
+export { getGridPointsCache, saveGridPointsCache };
