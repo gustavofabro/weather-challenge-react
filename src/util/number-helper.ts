b0bd605d1f51042d3810500bcbd5b3f2311fe676
gustavofabro <gustavofabro.f@gmail.com>
2023-03-05 11:31:34 -0300

    feat/weather: fix lint

--- src/util/number-helper.ts
+++ src/util/number-helper.ts
@@ -1,7 +1,9 @@
-function truncDecimal(value: number, digitos = 4): string {
+const DEFAULT_TRUNC_PRECISION = 4;
+
+function truncDecimal(value: number, digits = DEFAULT_TRUNC_PRECISION): string {
   const valueAsString = `${value}`;
 
-  return `${valueAsString}`.slice(0, valueAsString.indexOf('.') + digitos + 1);
+  return `${valueAsString}`.slice(0, valueAsString.indexOf('.') + digits + 1);
 }
 
 export { truncDecimal };
